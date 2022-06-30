6878cbdc5c0357aa50478d68a2f2fbe3be4860fb
Jerry Seyi <jerryseyi68@gmail.com>
2022-06-30 10:23:11 +0100

    plus minus array

---
+++ plus_minus.php
@@ -0,0 +1,30 @@
+<?php
+
+function plusMinus($arr) {
+    $positive = 0;
+    $negative = 0;
+    $zero = 0;
+    $length = count($arr);
+
+    foreach ($arr as $value) {
+        if ($value === 0) {
+            $zero+=1;
+        } else if ($value > 0) {
+            $positive += 1;
+        } else {
+            $negative += 1;
+        }
+    }
+
+    $newArr = [$positive, $negative, $zero];
+
+    foreach ($newArr as $val) {
+        echo number_format($val/$length, 6);
+        echo "\n";
+    }
+}
+
+$arr = [-1, 0, 2, 3, 4];
+
+plusMinus($arr);
+?>
