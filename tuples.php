85c6b9ff0b1374c5c4b228a5274a59c69e9e7808
Jerry Seyi <jerryseyi68@gmail.com>
2022-07-03 14:56:45 +0100

    get mid array

---
+++ tuples.php
@@ -0,0 +1,26 @@
+<?php
+
+function makeTuples(array $input): array
+{
+    $answer = [];
+    $length = count($input);
+
+    for ($i = 0; $i < $length; $i++) {
+        for ($j = 0; $j < $length; $j++) {
+            $answer[][] = $input[$i].$input[$j];
+//            array_push($answer, $input[$i], $input[$j]);
+        }
+    }
+    return $answer;
+}
+
+print_r(makeTuples([1, 2, 3]));
+
+// O n^2
+
+function getMiddleArray(array $arr)
+{
+    return $arr[floor(count($arr) / 2)];
+}
+
+// constant time (0 of 1)
