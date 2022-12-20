9c659ba7987cb21ce5b9fe1f5639b2d3b9d2df03
jerry seyi <jerryseyi68@gmail.com>
2022-12-20 11:41:56 +0100

    last bubblesort

--- src/sort/bubblesort.php
+++ src/sort/bubblesort.php
@@ -67,6 +67,32 @@ function finalBubbleSort(array $arr): array
     return $arr;
 }
 
+function lastBubbleSort(array $arr): array
+{
+    $len = count($arr);
+    $count = 0;
+    $bound = $len - 1;
+
+    for ($i = 0; $i < $len; $i++) {
+        $swapped = false;
+        $newBound = 0;
+        for ($j = 0; $j < $bound; $j++) {
+            $count++;
+            if ($arr[$j] > $arr[$j + 1]) {
+                $temp = $arr[$j + 1];
+                $arr[$j + 1] = $arr[$j];
+                $arr[$j] = $temp;
+                $swapped = true;
+                $newBound = $j;
+            }
+        }
+        $bound = $newBound;
+        if (!$swapped) break;
+    }
+    echo $count . "\n";
+    return $arr;
+}
+
 $arr = [20, 45, 93, 67, 10, 97, 52, 88, 33, 92];
-$sortedArr = bubbleSort($arr);
+$sortedArr = finalBubbleSort($arr);
 echo implode(",", $sortedArr);
