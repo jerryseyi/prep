fea15ca251fde36396ff5710691dbf1feeaeeef6
jerry seyi <jerryseyi68@gmail.com>
2022-12-20 11:21:56 +0100

    final improved version of bubblesort

--- src/sort/bubblesort.php
+++ src/sort/bubblesort.php
@@ -17,6 +17,10 @@ function bubbleSort(array $arr): array
     return $arr;
 }
 
+/**
+ * @param array $arr
+ * @return array
+ */
 function secondBubbleSort(array $arr): array
 {
     $len = count($arr);
@@ -24,6 +28,32 @@ function secondBubbleSort(array $arr): array
     for ($i = 0; $i < $len; $i++) {
         $swapped = false;
         for ($j = 0; $j < $len - 1; $j++) {
+            if ($arr[$j] > $arr[$j + 1]) {
+                $temp = $arr[$j + 1];
+                $arr[$j + 1] = $arr[$j];
+                $arr[$j] = $temp;
+                $swapped = true;
+            }
+        }
+        if (!$swapped) break;
+    }
+
+    return $arr;
+}
+
+/**
+ * final improved version of bubbleSort
+ *
+ * @param array $arr
+ * @return array
+ */
+function finalBubbleSort(array $arr): array
+{
+    $len = count($arr);
+
+    for ($i = 0; $i < $len; $i++) {
+        $swapped = false;
+        for ($j = 0; $j < $len - $i - 1; $j++) {
             if ($arr[$j] > $arr[$j + 1]) {
                 $temp = $arr[$j + 1];
                 $arr[$j + 1] = $arr[$j];
